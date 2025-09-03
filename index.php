d6f2d2134734f5433fc9bcb8ccad33bb6e9085a5
wrootz <admin@haut.com.br>
2025-09-02 21:37:47 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - HautForms</title>
+    <link rel="stylesheet" href="[https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css](https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css)">
+</head>
+<body>
+    <section class="hero is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-black">Login</h3>
+                    <hr class="login-hr">
+                    <p class="subtitle has-text-black">Por favor, faça login para continuar.</p>
+                    <div class="box">
+                        <?php if (isset($error)) { ?>
+                            <div class="notification is-danger"><?php echo $error; ?></div>
+                        <?php } ?>
+                        <form action="index.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" type="email" name="email" placeholder="Seu E-mail Corporativo" autofocus="" required>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input class="input is-large" type="password" name="senha" placeholder="Sua Senha" required>
+                                </div>
+                            </div>
+                            <button class="button is-block is-info is-large is-fullwidth" type="submit">Entrar <i class="fa fa-sign-in" aria-hidden="true"></i></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
