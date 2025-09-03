eb7d4791854aa6c348695b57afc3a978fd439f2e
wrootz <admin@haut.com.br>
2025-09-02 21:30:04 -0300

    HautFORM

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Configurações do banco de dados
-// Renomeie este arquivo para config.php e preencha com suas credenciais
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'hautworl_uForms');
-define('DB_PASS', 'yEIMEZEvyszgxc4W');
-define('DB_NAME', 'hautworl_forms');
-?>
